58197e46586aeee199f274709967e9238fa56cd7
Shubham Tiwari <shubham.tiwari1097@gmail.com>
2020-08-21 02:00:40 +0530

    Adjust margins, paddings

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -470,7 +470,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60120" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61451" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
